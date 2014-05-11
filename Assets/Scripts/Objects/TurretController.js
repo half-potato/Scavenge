df408be73493d6dbe0df01237eca2a25dfec49be
zachooz <hoffking@gmail.com>
2014-05-10 22:27:26 -0700

    ss

--- Assets/Scripts/Objects/TurretController.js
+++ Assets/Scripts/Objects/TurretController.js
@@ -1,11 +1,11 @@
 ﻿#pragma strict
 var player:GameObject;
-public var extraRot:float;
 function Start () {
 
 }
 
 function Update () {
-	transform.LookAt(player.transform, Vector3.up);
-	transform.rotation.z+=extraRot;
+	if(player){
+		transform.LookAt(player.transform, transform.up);
+	}
 }
